6806c15e835ccf9acfdf3809496d4ba533df86ae
Simone Giusti <info@simonegiusti.it>
2022-07-06 12:31:36 +0200

    live completa

--- 1_ciclo_avanti/js/main.js
+++ 1_ciclo_avanti/js/main.js
@@ -1,16 +1,14 @@
 console.log('INIZIO');
 
 // pippo non professionale, ma ci capiamo ;-)
-/* for (let pippo = 150; pippo <= 350; pippo = pippo + 2) {
+for (let pippo = 150; pippo <= 350; pippo = pippo + 2) {
     console.log(pippo);
 }
- */
 
 
-/* for (let pluto = 200; pluto >= 0; pluto = pluto - 5) {
+for (let pluto = 200; pluto >= 0; pluto = pluto - 5) {
     console.log(pluto);
-} */
-
+} 
 
 for ( let tabellina = 1; tabellina <= 10; tabellina++) {
 
@@ -24,3 +22,16 @@ for ( let tabellina = 1; tabellina <= 10; tabellina++) {
 }
 
 console.log('FINE');
+
+
+
+
+/* operatore modulo: % 
+ restituisce il resto di una divisione, esempio 11 % 3, restituisce 2, poichè il 3 è contenuto 3 volte in 11. 
+ Il modulo sarà dato dalla sottrazione tra il numero stesso (11) e quante volte il divisore può essere contenuto nel numero stesso
+ Quindi 11 - ( 3 * 3 )  = 2;
+ 2 è il resto (modulo);
+https://it.wikipedia.org/wiki/Operazione_modulo
+ */
+
+
